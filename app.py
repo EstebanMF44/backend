37d01ac29fdacd1abbe4a91f8623e1e8e1a6afa3
Esteban Martin-Fréour <esteban.martin@etu.minesparis.psl.eu>
2025-03-31 22:54:34 +0200

    APP 1.0

--- app.py
+++ app.py
@@ -18,6 +18,51 @@ evenements_df = pd.read_csv(data / 'evenements_associations.csv')
 
 ## Vous devez ajouter les routes ici : 
 
+@app.route('/alive', methods=['GET'])
+def alive():
+  if request.method == 'GET':
+    if app.status_code == 200:
+       print(f"+200 OK : {serveur : alive}")
+  else:
+    return "Méthode non autorisée", 405
+  
+@app.route('/associations', methods=['GET'])
+def associations():
+  if request.method == 'GET':
+    l = associations_df['nom']
+    print(l)
+  else:
+    return "Méthode non autorisée", 405
+  
+@app.route('/association/<int:id>', methods=['GET'])
+def association(id):
+  if request.method == 'GET':
+    if id in associations():
+      pass
+    else:
+      print(f"404 Not Found : {error : Association not found}")
+  else:
+    return "Méthode non autorisée", 405
+  
+@app.route('/evenements', methods=['GET'])
+def evenements(id):
+  if request.method == 'GET':
+    l = evenements_df['nom']
+    print(l)
+  else:
+    return "Méthode non autorisée", 405
+  
+@app.route('/evenement/<int:id>', methods=['GET'])
+def association(id):
+  if request.method == 'GET':
+    if id in evenements():
+      pass
+    else:
+      print(f"404 Not Found : {error : Event not found}")
+  else:
+    return "Méthode non autorisée", 405
+
+
 
 
 if __name__ == '__main__':
